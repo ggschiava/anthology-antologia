8a7ab296836bc4cc300e2170a078e10f395b5e6b
califano <ggschiava@gmail.com>
2021-06-25 23:39:19 +0200

    Added more poems, updated script to create book_AnthologyAantologia

--- english_español/book_AnthologyAntologia/configurationCreateBook/createBook.sh
+++ english_español/book_AnthologyAntologia/configurationCreateBook/createBook.sh
@@ -3,4 +3,18 @@
 uber_jar=/home/g/Dropbox/cuentos/wordpressbackuptoother/target/scala-3.0.0/scala3-simple-assembly-0.1.0.jar
 conf=/home/g/Dropbox/cuentos/anthology-antologia/english_español/book_AnthologyAntologia/configurationCreateBook/createBook.conf
 
+warning_star="WARNING STAR if the setting is changed on conf the following code should be updated... basically is a bug wating to happen :("
+warging_end="WARNING END"
+
+echo $warning_star
+path_book_created=/home/g/Dropbox/cuentos/anthology-antologia/english_español/book_AnthologyAntologia/AnthologyAntologia.pdf 
+rm $path_book_created
+echo $warning_end
+
+
 scala $uber_jar $conf
+
+
+echo $warning_star
+evince $path_book_created
+echo $warning_end
